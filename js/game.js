72755bbdb5f1075b9aa8c6ab8415684089b47a65
AleksBurdeinyi <144951187+AleksBurdeinyi@users.noreply.github.com>
2024-03-18 17:03:21 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -280,10 +280,10 @@ document.getElementById("playAgainButton").addEventListener("click", function() 
 });
 
 document.getElementById("mainMenuButton").addEventListener("click", function() {
-    window.location.href = 'http://127.0.0.1:5500/game/welcome.html'; // Перехід на  головне меню
+    window.location.href = 'http://127.0.0.1:5500/game/index.html'; // Перехід на  головне меню
 });
 document.getElementById("goMenubutton").addEventListener("click", function() {
-    window.location.href = 'http://127.0.0.1:5500/game/welcome.html'; // Перехід на  головне меню
+    window.location.href = 'http://127.0.0.1:5500/game/index.html'; // Перехід на  головне меню
 });
 
 
